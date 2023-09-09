916e83f14129e18c3aec0d20215f7e889d8f077a
shabeebtk <shabeebtk768@gmail.com>
2023-09-09 13:46:09 +0530

    recovered pycopg

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -28,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = []
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
